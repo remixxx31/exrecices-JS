0d7c8a1fbab8b5a4f2117d4cb8ed7296e4b06252
rémi perray <remiperray@gmail.com>
2022-07-25 23:10:20 +0200

    ex17.js do and while

--- Maitrisez le javascript en 30 exercices pratiques/Exercice Facile/ex17js.js
+++ Maitrisez le javascript en 30 exercices pratiques/Exercice Facile/ex17js.js
@@ -3,8 +3,8 @@ let saisie = parseInt(readline.questionInt("Veuillez saisir le chiffre 3: "));
 
 // let inp = document.querySelector("body > input[type=text]").innerHTML;
 
-while (saisie !==3) {
+do {
   console.log("Veuillez recommencez");
   saisie = parseInt(readline.questionInt("Veuillez saisir le chiffre 3: "));
-}
+} while (saisie !== 3);
 console.log("Bonne réponse");
